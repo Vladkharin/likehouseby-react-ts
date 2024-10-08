70bde55332227673ef68e3a2b84459a63b5afc18
Vlad <koooornienko@gmail.com>
2024-10-08 15:47:12 +0500

    add google and yandex metrics

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,6 @@ import { Footer } from "./components/Footer";
 import { PaymentPage } from "./components/paymentPage/paymentPage";
 import { HousePage } from "./components/housePage/HousePage";
 
-import Helmet from "react-helmet";
-
 function App() {
   const [scroll, setScroll] = useState(0);
   const [mainPage] = useState("/");
@@ -31,20 +29,6 @@ function App() {
 
   return (
     <Router>
-      <Helmet>
-        {/* <!-- Google Tag Manager --> */}
-        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-TZDBGTVJ');`}</script>
-        {/* <!-- End Google Tag Manager --> */}
-
-        {/* <!-- Google Tag Manager (noscript) --> */}
-        <noscript>{`<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZDBGTVJ"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>`}</noscript>
-        {/* <!-- End Google Tag Manager (noscript) --> */}
-      </Helmet>
       <Header scroll={scroll} mainPage={mainPage} setBodyStyle={setBodyStyle} />
       <Routes>
         <Route path={"/:anchor?"} element={<MainPage setBodyStyle={setBodyStyle} />} />
